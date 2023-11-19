7c97caba95b5228a88a7695ce10dc8011aec17d7
alejandro <josued.ad13@gmail.com>
2023-11-19 16:07:13 -0600

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: 'http://alejandrojosue.github.io/myTemplates',
-  server: {
-    port: 200
-  }
 })
